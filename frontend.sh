f3233826e6072eed8d81386c242571f1c77fab9a
pappik <paparao.prema75@gmail.com>
2025-03-12 20:45:58 -0500

    till conf file copy for frontend

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,17 @@
 source common.sh
 
 msg "install nginx"
-dnf install nginx -y
+dnf install nginx -y &>> /tmp/expense.log
+
+msg "remove existing html"
+rm -rf /usr/share/nginx/html/*
+
+msg "download the frontend"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+
+msg "extract the frontend"
+cd /usr/share/nginx/html
+unzip /tm/frontend.zip
+
+msg "add conf file to location"
+cp /files/expense.conf /etc/nginx/default.d/expense.conf
